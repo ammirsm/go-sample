4a81aa138de063e48a4d23912c9ce8d54af158dc
Amir Hossein Saeid Mehr <amir.saiedmehr@gmail.com>
2019-05-15 02:15:02 +0430

    distinct urls to other file

---
+++ urls.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", mainPage).Methods("GET")
+	myRouter.HandleFunc("/cards", allCards).Methods("GET")
+	myRouter.HandleFunc("/transactions/add_tag/", addTagForTransactions).Methods("POST")
+	myRouter.HandleFunc("/transactions/delete_tag/{tag_id:[0-9]+}", deleteTagForTransactions).Methods("DELETE")
+	myRouter.HandleFunc("/transactions/{card_id:[0-9]+}/", allTransactions).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
+}
