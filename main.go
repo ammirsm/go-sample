f04ed147daa9a59126c047d989a4fb29c92e59b7
Amir Hossein Saeid Mehr <amir.saiedmehr@gmail.com>
2019-05-15 02:06:17 +0430

    add and delete for tags in api

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ import (
 )
 import "strconv"
 
+type JsonResponse struct {
+	Code	int
+	Description	string
+}
 
 
 func allTransactions(w http.ResponseWriter, r *http.Request) {
@@ -43,7 +47,7 @@ func allCards(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 	var allCards []Card
 
-	// we should got user from token but I just mock this up
+	//TODO: we should got user from token but I just mock this up
 	var user User
 	db.First(&user,User{Email:"amir.saiedmehr@gmail.com"})
 
@@ -61,6 +65,58 @@ func allCards(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(allCards)
 }
 
+func addTagForTransactions(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+
+	var tag Tag
+	err := decoder.Decode(&tag)
+	if err != nil {
+		panic(err)
+	}
+
+	db = openDb(db)
+	defer db.Close()
+
+	var transaction Transaction
+	db.First(&transaction, 1)
+	var relatedTags []Tag
+	db.Model(&transaction).Related(&relatedTags)
+
+	for i := range relatedTags{
+		if relatedTags[i].Name == tag.Name{
+			w.WriteHeader(http.StatusConflict)
+			json.NewEncoder(w).Encode(&JsonResponse{409,"You've added this tag to this transaction recently"})
+			return
+		}
+	}
+
+	//TODO: Should check the user that is requesting is the owner of transaction or not
+
+	db.Create(&tag)
+
+	json.NewEncoder(w).Encode(tag)
+}
+
+func deleteTagForTransactions(w http.ResponseWriter, r *http.Request) {
+	db = openDb(db)
+	defer db.Close()
+
+	vars := mux.Vars(r)
+	TagId := vars["tag_id"]
+	TagIdInt, _ := strconv.ParseInt(TagId, 0, 64)
+
+
+	var tag Tag
+	db.First(&tag,TagIdInt)
+
+	//TODO: Should check the user that is requesting is the owner of transaction or not
+
+	db.Delete(&tag)
+
+	w.WriteHeader(http.StatusGone)
+	json.NewEncoder(w).Encode(&JsonResponse{410,"Deleted successfully"})
+}
+
 func mainPage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Hello world, this is wealth ethical API main page :)")
 }
@@ -69,10 +125,12 @@ func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", mainPage).Methods("GET")
 	myRouter.HandleFunc("/cards", allCards).Methods("GET")
+	myRouter.HandleFunc("/transactions/add_tag/", addTagForTransactions).Methods("POST")
+	myRouter.HandleFunc("/transactions/delete_tag/{tag_id:[0-9]+}", deleteTagForTransactions).Methods("DELETE")
 	myRouter.HandleFunc("/transactions/{card_id:[0-9]+}/", allTransactions).Methods("GET")
-	myRouter.HandleFunc("/transactions/add_tag/{transaction_id:[0-9]+}/", allTransactions).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
+
 
 func main() {
 	fmt.Println("Server is up")
