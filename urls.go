1e543e45c2818f11617f0bdb3990c402cb0bd46d
Amir Hossein Saeid Mehr <amir.saiedmehr@gmail.com>
2019-05-27 15:14:30 +0430

    urls update with /

--- urls.go
+++ urls.go
@@ -10,9 +10,9 @@ import (
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", mainPage).Methods("GET")
-	myRouter.HandleFunc("/cards", allCards).Methods("GET")
+	myRouter.HandleFunc("/cards/", allCards).Methods("GET")
 	myRouter.HandleFunc("/transactions/add_tag/", addTagForTransactions).Methods("POST")
-	myRouter.HandleFunc("/transactions/delete_tag/{tag_id:[0-9]+}", deleteTagForTransactions).Methods("DELETE")
+	myRouter.HandleFunc("/transactions/delete_tag/{tag_id:[0-9]+}/", deleteTagForTransactions).Methods("DELETE")
 	myRouter.HandleFunc("/transactions/{account_id:[0-9]+}/", allTransactions).Methods("GET")
 	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), myRouter))
 }
